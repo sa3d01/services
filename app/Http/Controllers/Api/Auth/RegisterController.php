29482922b287799607393e999e968eb442b8352f
sa3d01 <sa3dsalem01@gmail.com>
2021-06-06 15:23:20 +0200

    gitignore

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Api\MasterController;
 use App\Http\Requests\Api\Auth\UserRegisterationRequest;
 use App\Models\Social;
 use App\Models\User;
-use App\Traits\UserEmailVerificationTrait;
 use App\Traits\UserPhoneVerificationTrait;
 use Spatie\Permission\Models\Role;
 
@@ -19,8 +18,8 @@ class RegisterController extends MasterController
         $data = $request->validated();
         $data['last_ip'] = $request->ip();
         $user = User::create($data);
-        $data['user_id']=$user->id;
-        $socials=Social::create($data);
+        $data['user_id'] = $user->id;
+        Social::create($data);
         $user->refresh();
         $role = Role::findOrCreate($user->type);
         $user->assignRole($role);
