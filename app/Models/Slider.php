a1adab99af66ab694907847c3326bb3912f54022
sa3d01 <sa3dsalem01@gmail.com>
2021-06-13 15:11:59 +0200

    edits

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use App\Traits\ModelBaseFunctions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Slider extends Model
 {
     use HasFactory;
-    private $route='slider';
     private $images_link='media/images/slider/';
     protected $fillable = [
         'type',
@@ -27,4 +26,44 @@ class Slider extends Model
         'start_date' => 'date',
         'end_date' => 'date',
     ];
+    private function upload_file($file)
+    {
+        $filename = Str::random(10) . '.' . $file->getClientOriginalExtension();
+        $file->move($this->images_link, $filename);
+        return $filename;
+    }
+
+    function deleteFileFromServer($filePath)
+    {
+        if ($filePath != null) {
+            if (file_exists($filePath)) {
+                unlink($filePath);
+            }
+        }
+    }
+
+    protected function setImageAttribute()
+    {
+        $image = request('image');
+        $filename = null;
+        if (is_file($image)) {
+            $filename = $this->upload_file($image);
+        } elseif (filter_var($image, FILTER_VALIDATE_URL) === True) {
+            $filename = $image;
+        }
+        $this->attributes['image'] = $filename;
+    }
+
+    protected function getImageAttribute()
+    {
+        $dest = $this->images_link;
+        try {
+            if ($this->attributes['image'])
+                return asset($dest) . '/' . $this->attributes['image'];
+            return asset('media/images/image.jpeg');
+        } catch (\Exception $e) {
+            return asset('media/images/image.jpeg');
+        }
+    }
+
 }
