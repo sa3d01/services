7fd2826e802d12e50aa430eff0c96bfa6940e020
sa3d01 <sa3dsalem01@gmail.com>
2021-08-28 22:07:53 +0200

    sms

--- app/Traits/UserPasswordResetTrait.php
+++ app/Traits/UserPasswordResetTrait.php
@@ -9,12 +9,40 @@ trait UserPasswordResetTrait
 {
     protected function createPasswordResetCodeForUser($user)
     {
+        $code=rand(1111,9999);
         $data = [
             'phone' => $user->phone,
-            'token' => 2021,//rand(1111, 9999),
+            'token' => $code,
             'expires_at' => Carbon::now()->addMinutes(10),
         ];
         PasswordReset::create($data);
+        $this->sendMessage('كود التفعيل الخاص بمقدمي الخدمات :'.$code,$user->phone);
         return $data;
     }
+    private function sendMessage($message, $phoneNumber)
+    {
+        $getdata = http_build_query(
+            $fields = array(
+                "Username" => "0550085823",
+                "Password" => "Aa12345678",
+                "Message" => $message,
+                "RecepientNumber" => $phoneNumber,
+                "ReplacementList" => "",
+                "SendDateTime" => "0",
+                "EnableDR" => False,
+                "Tagname" => "خدماتك",
+                "VariableList" => "0"
+            ));
+        $opts = array('http' =>
+            array(
+                'method' => 'GET',
+                'header' => 'Content-type: application/x-www-form-urlencoded',
+
+            )
+        );
+        $context = stream_context_create($opts);
+        $results = file_get_contents('http://api.yamamah.com/SendSMSV2?' . $getdata, false, $context);
+        return $results;
+    }
+
 }
--- app/Traits/UserPhoneVerificationTrait.php
+++ app/Traits/UserPhoneVerificationTrait.php
@@ -11,15 +11,42 @@ trait UserPhoneVerificationTrait
 
     protected function createPhoneVerificationCodeForUser($user)
     {
+        $code=rand(1111,9999);
         $data = [
             'user_id' => $user->id,
             'phone' => $user->phone,
-            'code' => 2021,//rand(1111, 9999),
+            'code' => $code,
             'expires_at' => Carbon::now()->addMinutes(15),
         ];
         PhoneVerificationCode::create($data);
-        //todo : send sms
+        $this->sendMessage('كود التفعيل الخاص بمقدمي الخدمات :'.$code,$user->phone);
         return $data;
+    }
+
+    private function sendMessage($message, $phoneNumber)
+    {
+        $getdata = http_build_query(
+            $fields = array(
+                "Username" => "0550085823",
+                "Password" => "Aa12345678",
+                "Message" => $message,
+                "RecepientNumber" => $phoneNumber,
+                "ReplacementList" => "",
+                "SendDateTime" => "0",
+                "EnableDR" => False,
+                "Tagname" => "خدماتك",
+                "VariableList" => "0"
+            ));
+        $opts = array('http' =>
+            array(
+                'method' => 'GET',
+                'header' => 'Content-type: application/x-www-form-urlencoded',
+
+            )
+        );
+        $context = stream_context_create($opts);
+        $results = file_get_contents('http://api.yamamah.com/SendSMSV2?' . $getdata, false, $context);
+        return $results;
     }
 
 }
